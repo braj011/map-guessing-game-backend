80d9a265c27a2e9dfbe7139a331a2bbd9be34863
Ciaran M <cmorinan@gmail.com>
2018-10-25 14:03:43 +0100

    bug fix

--- app/controllers/api/v1/areas_controller.rb
+++ app/controllers/api/v1/areas_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::AreasController < ApplicationController
 
   def index
     @areas = Area.order("RANDOM()").first(100)
-    @areas.uniq(&:constituency).first(10)
+    @areas = @areas.uniq(&:constituency).first(10)
     render json: @areas
   end
 
