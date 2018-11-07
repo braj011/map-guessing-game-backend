6dca8ab48f2aca78623b54303a295660aaf0f6cf
Ciaran M <cmorinan@gmail.com>
2018-11-07 13:41:17 +0000

    amended cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://ldn-knowledge.herokuapp.com'
+    origins 'https://ldn-knowledge.herokuapp.com'
 
     resource '*',
       headers: :any,
